c773be3ea3086f6b295598ccad329522fee2be2a
Anders Miltner <Anders.Miltner@gmail.com>
2020-10-13 14:34:58 -0500

    actual fix

--- code/myStdlib/simpleFile.ml
+++ code/myStdlib/simpleFile.ml
@@ -4,6 +4,11 @@ let write_to_file
     ~(fname:string)
     ~(contents:string)
   : unit =
+  let fd = Unix.openfile ~mode:[O_WRONLY;O_CREAT;O_TRUNC] fname in
+  let pos = 0 in
+  let len = 1 in
+  let _ = Unix.single_write_substring ~pos ~len fd ~buf:"a" in
+  Unix.close fd;
   Out_channel.write_all fname ~data:contents
 
 let read_from_file
