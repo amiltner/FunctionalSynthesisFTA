c0711749834cad0124fe46ecf6ca610763aeb240
Anders Miltner <Anders.Miltner@gmail.com>
2020-10-13 14:08:57 -0500

    update simplefile to explicitly put in position and length parameters

--- code/myStdlib/simpleFile.ml
+++ code/myStdlib/simpleFile.ml
@@ -5,7 +5,9 @@ let write_to_file
     ~(contents:string)
   : unit =
   let fd = Unix.openfile ~mode:[O_WRONLY;O_CREAT;O_TRUNC] fname in
-  let _ = Unix.single_write_substring fd ~buf:contents in
+  let pos = 0 in
+  let len = String.length contents in
+  let _ = Unix.single_write_substring ~pos ~len fd ~buf:contents in
   Unix.close fd
 
 let read_from_file
